d32e6b30b1903b2fac74a434d95a7c3576206f3d
DigitalDreamscape1 <digitaldreamscape1@outlook.com>
2023-03-15 11:34:14 +0000

    Fixes
    Added transition
    removed mouse lock

--- Assets/scripts/roomTrans/minigameTrans.cs
+++ Assets/scripts/roomTrans/minigameTrans.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class minigameTrans : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void OnMouseDown()
     {
         SceneManager.LoadScene("miniGame2");
